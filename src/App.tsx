dcca8135fa0552b16dc4c08180eba5315c74ca83
Max Hirchenko <resoursa@hotmail.com>
2025-07-09 16:38:05 +0300

    Inittial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-red-600/100">Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
